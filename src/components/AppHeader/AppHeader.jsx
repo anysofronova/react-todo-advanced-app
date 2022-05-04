9db6c3da6dadb6cb9c15a7b04b896b4308d7aa04
Anna <anysofronova@yandex.ru>
2022-05-04 13:05:52 +0300

    AppHeader Refactoring

--- src/components/AppHeader/AppHeader.jsx
+++ src/components/AppHeader/AppHeader.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import cn from "classnames";
+import style from "./AppHeader.module.css";
 
-const AppHeader = () => {
-  return <h1>My ToDoApp List</h1>;
+const AppHeader = ({ toDo, done }) => {
+  return (
+    <div className={cn(style.AppHeader)}>
+      <h1>My ToDoApp List</h1>
+      <h2>
+        {toDo} more to do, {done} done
+      </h2>
+    </div>
+  );
 };
 
 export default AppHeader;
