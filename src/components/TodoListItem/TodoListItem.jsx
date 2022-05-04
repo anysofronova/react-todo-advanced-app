12f06532fc8bac6d13c30ea86099181756d1a493
Anna <anysofronova@yandex.ru>
2022-05-04 14:56:31 +0300

    Important and Done task

--- src/components/TodoListItem/TodoListItem.jsx
+++ src/components/TodoListItem/TodoListItem.jsx
@@ -2,22 +2,51 @@ import React from "react";
 import style from "./TodoListItem.module.css";
 import cn from "classnames";
 import { BsExclamationLg, BsTrash } from "react-icons/bs";
+import * as PropTypes from "prop-types";
 
-const TodoListItem = ({ label, important = false }) => {
-  return (
-    <div className={cn(style.TodoListItem)}>
-      <span className={cn(important && style.important, style.label)}>
-        {label}
-      </span>
+export default class TodoListItem extends React.Component {
+  state = {
+    done: false,
+    important: false,
+  };
+  onDoneTask = () => {
+    this.setState(({ done }) => {
+      return { done: !done };
+    });
+  };
+  onImportantTask = () => {
+    this.setState(({ important }) => {
+      return { important: !important };
+    });
+  };
+  render() {
+    const { label, id, onDeleted } = this.props;
+    const { done, important } = this.state;
+    return (
+      <div className={cn(style.TodoListItem)}>
+        <span
+          className={cn(
+            important && style.important,
+            done && style.done,
+            style.label
+          )}
+          onClick={this.onDoneTask}
+        >
+          {label}
+        </span>
 
-      <button className={cn(style.btn, "btn")}>
-        <BsExclamationLg />
-      </button>
-      <button className={cn(style.btn, "btn")}>
-        <BsTrash />
-      </button>
-    </div>
-  );
+        <button className={cn(style.btn, "btn")} onClick={this.onImportantTask}>
+          <BsExclamationLg />
+        </button>
+        <button className={cn(style.btn, "btn")} onClick={() => onDeleted(id)}>
+          <BsTrash />
+        </button>
+      </div>
+    );
+  }
+}
+
+TodoListItem.propTypes = {
+  label: PropTypes.string,
+  important: PropTypes.bool,
 };
-
-export default TodoListItem;
