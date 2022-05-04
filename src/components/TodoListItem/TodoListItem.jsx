d6e9be427a5f514841bd181cc83b2455d4d273fb
Anna <anysofronova@yandex.ru>
2022-05-04 13:06:42 +0300

    TodoListItem Refactoring

--- src/components/TodoListItem/TodoListItem.jsx
+++ src/components/TodoListItem/TodoListItem.jsx
@@ -1,8 +1,23 @@
 import React from "react";
 import style from "./TodoListItem.module.css";
+import cn from "classnames";
+import { BsExclamationLg, BsTrash } from "react-icons/bs";
 
 const TodoListItem = ({ label, important = false }) => {
-  return <span className={important && style.important}>{label}</span>;
+  return (
+    <div className={cn(style.TodoListItem)}>
+      <span className={cn(important && style.important, style.label)}>
+        {label}
+      </span>
+
+      <button className={cn(style.btn, "btn")}>
+        <BsExclamationLg />
+      </button>
+      <button className={cn(style.btn, "btn")}>
+        <BsTrash />
+      </button>
+    </div>
+  );
 };
 
 export default TodoListItem;
